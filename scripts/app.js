c9c5a0963ee33d1d3d3a748ed01e5bc7d5830fb4
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2025-04-02 13:21:25 -0600

    finished tablet now to desktop

--- scripts/app.js
+++ scripts/app.js
@@ -1,76 +1,63 @@
 // Fetch both containers ID
 
-const  inputBox = document.getElementById('input-box');
- const taskContainer = document.getElementById("task-list-container");
+const inputBox = document.getElementById('input-box');
+const taskContainer = document.getElementById("task-list-container");
 
-//  Create a function for adding tasks, once you click the button add,  it should proceed to add a another container of tasks, then a create a <li> element.
+// Create a function for adding tasks, once you click the button add, it should proceed to add another container of tasks, then create a <li> element.
 
-function addTask(){
+function addTask() {
     const task = inputBox.value.trim();
-    if (!task){
-        alert('Please write a task!')
+    if (!task) {
+        alert('Please write a task!');
         return;
     }
 
-    // Once created a new element create a inner html
-    const li =  document.createElement("li");
-    
+    // Create a new <li> element
+    const li = document.createElement("li");
+
+    // Set the inner HTML of the <li> element
     li.innerHTML = `
     <label>
-    <input class="checkbox" type = "checkbox">
-    <span class="task-font">${task}</span>
-    
+        <input class="checkbox" type="checkbox">
+        <span class="task-font">${task}</span>
     </label>
-    
-    <span class="edit-btn">Edit</span>
-    <span class="delete-btn">Delete</span>
-    
+    <div class="button-container">
+        <span class="edit-btn">Edit</span>
+        <span class="delete-btn">Delete</span>
+    </div>
     `;
 
     const checkbox = li.querySelector('input');
     const editBtn = li.querySelector('.edit-btn');
-    const taskSpan = li.querySelector('span');
+    const taskSpan = li.querySelector('.task-font');
     const deleteBtn = li.querySelector('.delete-btn');
 
-    
     taskContainer.appendChild(li);
-    inputBox.value ="";
-    // Start to make sure the buttons work, by doing an event listener
-    
-    checkbox.addEventListener("click", function() {
+    inputBox.value = "";
+
+    // Add event listeners
+    checkbox.addEventListener("click", function () {
         li.classList.toggle("completed", checkbox.checked);
-    
         updateCounters();
     });
-    // make an event listener for the checkbox, when the task is completed it should create a line thorugh the middle. Must do a css but first we will create a toggle
-    
-    // Make an event listener for the edit button, that you can also update
-    
-    editBtn.addEventListener("click", function(){
+
+    editBtn.addEventListener("click", function () {
         const update = prompt("Edit Task:", taskSpan.textContent);
-        if (update !== null){
+        if (update !== null) {
             taskSpan.textContent = update;
             li.classList.remove("completed");
-    
             checkbox.checked = false;
-            // updateCounters();
+            updateCounters();
         }
-    
     });
 
-    // Delete button functionality
-deleteBtn.addEventListener("click", function() {
-    if (confirm("Are you sure you want to delete this task? ")) {
-        li.remove();
-        updateCounters();
-    }
-
-
-});
+    deleteBtn.addEventListener("click", function () {
+        if (confirm("Are you sure you want to delete this task?")) {
+            li.remove();
+            updateCounters();
+        }
+    });
 }
-
-
-
 
 // Task functionality, to track the tasks that are complete and uncompleted the counter makes that happen
 
@@ -78,14 +65,10 @@ const completedCounter = document.getElementById("completed-counter");
 const uncompletedCounter = document.getElementById("uncompleted-counter");
 
 function updateCounters() {
-
     const completedTasks = document.querySelectorAll(".completed").length;
     const uncompletedTasks = document.querySelectorAll("li:not(.completed)").length;
-completedCounter.textContent = completedTasks;
-uncompletedCounter.textContent = uncompletedTasks;
-
-
-
+    completedCounter.textContent = completedTasks;
+    uncompletedCounter.textContent = uncompletedTasks;
 }
 
 
